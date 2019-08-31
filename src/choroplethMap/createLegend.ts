64251130a3c397bfabfa8b369c6610e827d123d3
Artyom Guryanov <guryanov.artyom@gmail.com>
2019-08-31 14:10:34 +0600

    legend: scope between min and max values; 4 scope parts with accurate ticks

--- src/choroplethMap/createLegend.ts
+++ src/choroplethMap/createLegend.ts
@@ -1,36 +1,37 @@
 import * as d3 from 'd3';
 import usaEducationMapStore from '../stores/UsaEducationMapStore';
 
-const scaleSizes = {
-  width: 400,
-  height: 110,
-  padding: 60,
-  parts: 4,
-};
-
 interface EducationRangeScales {
   xScale: d3.ScaleLinear<number, number>;
   colorScale: d3.ScaleLinear<number, number>;
 }
 
+const legendScale = {
+  width: 400,
+  height: 50,
+  padding: {
+    horizontal: 10,
+    vertical: 25,
+  },
+  parts: 4,
+};
+
 function createEducationRangeScales(): EducationRangeScales {
-  const { width } = scaleSizes;
-  const { countiesEducation } = usaEducationMapStore;
-  let minEducationLevel = d3.min(countiesEducation, (d) => d.bachelorsOrHigher);
-  let maxEducationLevel = d3.max(countiesEducation, (d) => d.bachelorsOrHigher);
+  const { width } = legendScale;
+  const { minEducationLevelInt, maxEducationLevelInt } = usaEducationMapStore;
   let xScale = d3
     .scaleLinear()
     .domain([
-      minEducationLevel,
-      maxEducationLevel,
+      minEducationLevelInt,
+      maxEducationLevelInt,
     ])
     .range([0, width]);
 
   let colorScale = d3
     .scaleQuantize()
     .domain([
-      minEducationLevel,
-      maxEducationLevel,
+      minEducationLevelInt,
+      maxEducationLevelInt,
     ])
     .range([
       'rgb(229, 245, 224)', 
@@ -46,13 +47,21 @@ function createEducationRangeScales(): EducationRangeScales {
 }
 
 function createEducationRangeAxes(svg, { xScale }) {
-  const { height, padding } = scaleSizes;
-  let xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+  const { height, padding: { horizontal: paddingHorizontal } } = legendScale;
+  const { minEducationLevelInt, maxEducationLevelInt } = usaEducationMapStore;
+  const educationStep = (maxEducationLevelInt - minEducationLevelInt) / legendScale.parts;
+  const xAxisTickValues = Array
+    .from({ length: legendScale.parts + 1})
+    .map((_, i) => minEducationLevelInt + educationStep * i);
+  let xAxis = d3
+    .axisBottom(xScale)
+    .ticks(1, "s")
+    .tickValues(xAxisTickValues);
 
   svg
     .append('g')
     .attr('id', 'legend-x-axis')
-    .attr('transform', `translate(0, ${height - padding})`)
+    .attr('transform', `translate(${paddingHorizontal}, ${height})`)
     .call(xAxis);
 
   return { xAxis };
@@ -65,29 +74,37 @@ interface EducationRange {
 }
 
 function createEducationRange(): EducationRange {
+  const {
+    width,
+    height,
+    padding: {
+      horizontal: paddingHorizontal,
+      vertical: paddingVertical,
+    },
+    parts: legendScaleParts
+  } = legendScale;
+
   let svg = d3
     .select('#legend')
     .append('svg')
-    .attr('width', scaleSizes.width)
-    .attr('height', scaleSizes.height);
+    .attr('width', width + paddingHorizontal * 2)
+    .attr('height', height + paddingVertical);
 
   let scales = createEducationRangeScales();
   let axes = createEducationRangeAxes(svg, scales);
-
-  let minEducationLevel = d3.min(usaEducationMapStore.countiesEducation, (d) => d.bachelorsOrHigher);
-  let maxEducationLevel = d3.max(usaEducationMapStore.countiesEducation, (d) => d.bachelorsOrHigher);
-  let educationLevelStep = (maxEducationLevel - minEducationLevel) / scaleSizes.parts;
-  let educationLevelRange = Array.from({length: scaleSizes.parts}).map((_, i) => minEducationLevel + i * educationLevelStep);
+  const { minEducationLevelInt, maxEducationLevelInt } = usaEducationMapStore;
+  let educationLevelStep = (maxEducationLevelInt - minEducationLevelInt) / legendScaleParts;
+  let educationLevelRange = Array.from({length: legendScaleParts}).map((_, i) => minEducationLevelInt + i * educationLevelStep);
 
   svg
     .selectAll('rect')
     .data(educationLevelRange)
     .enter()
     .append('rect')
-    .attr('x', (d) => scales.xScale(d))
+    .attr('x', (d) => paddingHorizontal + scales.xScale(d))
     .attr('y', 0)
-    .attr('width', scaleSizes.width / scaleSizes.parts)
-    .attr('height', scaleSizes.height - scaleSizes.padding)
+    .attr('width', width / legendScaleParts)
+    .attr('height', legendScale.height)
     .attr('fill', (d) => scales.colorScale(d));
 
   return {
--- src/stores/UsaEducationMapStore.ts
+++ src/stores/UsaEducationMapStore.ts
@@ -1,6 +1,23 @@
+import { min, max } from 'd3';
 import { IEducation, IUsaGeopath } from "../types";
 
 class UsaEducationMapStore {
+  get minEducationLevel() {
+    return min(this.countiesEducation, (d) => d.bachelorsOrHigher);
+  }
+
+  get minEducationLevelInt() {
+    return Math.floor(this.minEducationLevel);
+  }
+
+  get maxEducationLevel() {
+    return max(this.countiesEducation, (d) => d.bachelorsOrHigher);
+  }
+
+  get maxEducationLevelInt() {
+    return Math.ceil(this.maxEducationLevel);
+  }
+
   countiesEducation: IEducation;
   usaGeopath: IUsaGeopath;
 }
